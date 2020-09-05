e424320ff8a5aedaa979b8cb8258c0cbf5189e23
Aditi Pawar <30751634+PawarAditi@users.noreply.github.com>
2020-09-05 17:49:42 +0530

    Add files via upload

---
+++ Movie/app/src/main/java/com/example/movie/DetailActivity.java
@@ -0,0 +1,159 @@
+package com.example.movie;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.movie.api.Client;
+import com.example.movie.api.Service;
+import com.example.movie.model.Movie;
+import com.example.movie.model.Trailer;
+import com.example.movie.model.TrailerResponse;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.snackbar.Snackbar;
+
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DetailActivity extends AppCompatActivity {
+    TextView nameOfMovie, plotSynopsis, userRating, releaseDate;
+    ImageView imageView;
+    private RecyclerView recyclerView;
+    private List<Trailer> trailerList;
+    private Movie favorite;
+    private final AppCompatActivity activity = DetailActivity.this;
+
+    android.graphics.Movie movie;
+    String thumbnail, movieName, synopsis, rating, dateOfRelease;
+    int movie_id;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        initCollapsingToolbar();
+
+
+        nameOfMovie = (TextView) findViewById(R.id.title);
+        plotSynopsis = (TextView) findViewById(R.id.plotsynopsis);
+        userRating = (TextView) findViewById(R.id.userrating);
+        releaseDate = (TextView) findViewById(R.id.releasedate);
+
+        Intent intentThatStartedThisActivity = getIntent();
+        if (intentThatStartedThisActivity.hasExtra("movies")){
+
+            movie = getIntent().getParcelableExtra("movies");
+            String poster = "https://image.tmdb.org/t/p/w500" + thumbnail;
+
+            Glide.with(this)
+                    .load(poster)
+                    .placeholder(R.drawable.load)
+                    .into(imageView);
+
+            nameOfMovie.setText(movieName);
+            plotSynopsis.setText(synopsis);
+            userRating.setText(rating);
+            releaseDate.setText(dateOfRelease);
+
+        }else{
+            Toast.makeText(this, "No API Data", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+
+    private void initCollapsingToolbar(){
+        final CollapsingToolbarLayout collapsingToolbarLayout =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(" ");
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.setExpanded(true);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener(){
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset){
+
+
+                if (scrollRange == -1){
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0){
+                    collapsingToolbarLayout.setTitle(getString(R.string.movie_details));
+                    isShow = true;
+                }else if (isShow){
+                    collapsingToolbarLayout.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
+    }
+
+    private void initViews(){
+
+
+    }
+
+    private void loadJSON(){
+
+        try{
+            if (BuildConfig.THE_MOVIE_DB_API_TOKEN.isEmpty()){
+                Toast.makeText(getApplicationContext(), "Please obtain your API Key from themoviedb.org", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Client Client = new Client();
+            Service apiService = Client.getClient().create(Service.class);
+            Call<TrailerResponse> call = apiService.getMovieTrailer(movie_id, BuildConfig.THE_MOVIE_DB_API_TOKEN);
+            call.enqueue(new Callback<TrailerResponse>() {
+                @Override
+                public void onResponse(Call<TrailerResponse> call, Response<TrailerResponse> response) {
+                    List<Trailer> trailer = response.body().getResults();
+                    recyclerView.setAdapter(new TrailerAdapter(getApplicationContext(), trailer));
+                    recyclerView.smoothScrollToPosition(0);
+                }
+
+                @Override
+                public void onFailure(Call<TrailerResponse> call, Throwable t) {
+                    Log.d("Error", t.getMessage());
+                    Toast.makeText(DetailActivity.this, "Error fetching trailer data", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+        }catch (Exception e){
+            Log.d("Error", e.getMessage());
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void saveFavorite(){
+
+    }
+
+
+}
